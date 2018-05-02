c99f4fd512ffb2fa0e5dbc5c4ba59a81b76ee643
aluno <aluno@LAB2A-01>
2018-05-02 19:07:26 -0300

    explica ate a parte q foi feitaa Primeiro commit

---
+++ src/projetojavagit/App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetojavagit;
+
+/**
+ *
+ * @author aluno
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primeiro teste git");
+        // TODO code application logic here
+    }
+    
+}
